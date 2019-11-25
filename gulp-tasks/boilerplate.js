c5823601fc07fed1efa6a1a72e82bc0debe90be8
luisDPM <luis.padilla@dnamicworld.com>
2019-11-25 11:45:17 -0600

    updates in emails

--- gulp-tasks/boilerplate.js
+++ gulp-tasks/boilerplate.js
@@ -1,7 +1,7 @@
 module.exports = ({ gulp, args, config }) => {
   if (args.name) {
     const { emailSrc } = config;
-    return gulp.src(`.${emailSrc}/boilerplate/**/*`)
+    return gulp.src(`.${emailSrc}/cbp-assignment/**/*`)
       .pipe(gulp.dest(`.${emailSrc}/${args.name}`));
   }
   return false;
